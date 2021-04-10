083b73367f1b0081e2042223fcbd419eae88ba6f
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-04-10 09:09:15 +0530

    ADDED:Added 3 Floats number and searching max number

--- src/test/java/com/maximumtest/MaximumTest.java
+++ src/test/java/com/maximumtest/MaximumTest.java
@@ -21,11 +21,33 @@ public class MaximumTest {
     }
 
     @Test
-    public void givenMaximumNumberAtThirdPositionShouldReturnSameNumber() {
+    public void givenMaximumNumberAtSecondPositionShouldReturnSameNumber() {
         Integer [] intArray = {1 , 48 , 54};
         Integer result = maximumNumber.findMaximum(intArray);
         Assertions.assertEquals(Integer.valueOf(54), result);
     }
-    
-    
+
+    @Test
+    public void givenMaximumNumberAtSecondPositionShouldReturnSameNumber() {
+        Double [] doulbeArray = {5.8, 1.2, 3.4};
+        Double result = maximumTest.findMaximum(doulbeArray);
+        Assertions.assertEquals(Double.valueOf(5.8), result);
+    }
+
+    @Test
+    public void givenMaximumNumberAtSecondPositionShouldReturnSameNumber() {
+        Double [] doulbeArray = {5.2, 8.9, 2.1};
+        Double result = maximumTest.findMaximum(doulbeArray);
+        Assertions.assertEquals(Double.valueOf(8.9), result);
+    }
+
+
+    @Test
+    public void givenMaximumNumberAtSecondPositionShouldReturnSameNumber() {
+        Double [] doulbeArray = {5.9, 2.1, 7.6};
+        Double result = maximumTest.findMaximum(doulbeArray);
+        Assertions.assertEquals(Double.valueOf(7.6), result);
+
+    }
+
 }
